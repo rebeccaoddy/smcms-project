a7a8dcb00ac537652a34efa475de5a9d32f3922a
sam <samuelthorak@gmail.com>
2024-09-12 12:54:15 +0200

    login

--- HonsProj-master/src/App.js
+++ HonsProj-master/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Topbar from "./Scenes/Global/TopBar";
 import Sidebar from "./Scenes/Global/NavBar";
 import Faculty from "./Scenes/Overviews/Faculty";
@@ -14,21 +13,21 @@ import StudentDetails from "./Scenes/Overviews/Student/StudentDetails";
 import CourseDetail from "./Scenes/Overviews/Course/CourseDetail";
 import BarChart from "./components/BarChart";
 import ProgramDetail from "./Scenes/Overviews/Program/ProgramDetail";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
-
-//updated this to handle direct link and link with studentId 
 const SecondaryApp = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const studentId = queryParams.get('studentId');
-  
-  console.log("this is the student objectID:", studentId); // This should log the actual studentId
+
+  console.log("this is the student objectID:", studentId); 
 
   const iframeSrc = studentId
-    ? `http://localhost:5173/student-details?studentId=${studentId}` // Append studentId to the URL
+    ? `http://localhost:5173/student-details?studentId=${studentId}` 
     : `http://localhost:5173/cases`;
 
-  console.log("This is the iframe URL:", iframeSrc); // Debugging 
+  console.log("This is the iframe URL:", iframeSrc);
 
   return (
     <iframe
@@ -41,31 +40,33 @@ const SecondaryApp = () => {
 
 function App() {
   const [theme, colorMode] = useMode();
-  
+  const location = useLocation();
+
+  // Check if the current route is the login route
+  const isLoginRoute = location.pathname === "/";
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        
-          <div className="app">
-            <Sidebar />
-            <main className="content">
-              <Topbar />
-              <Routes>
-              <Route path="/" element={<Faculty />} />
-                <Route path="/Faculty" element={<Faculty />} />
-                <Route path="/Program" element={<Program />} />
-                <Route path="/Student" element={<Student />} />
-                <Route path="/students/:id" element={<StudentDetails />} />
-                <Route path="/Student/:id" element={<Studentpage />} />
-                <Route path="/Course" element={<Course />} />
-                <Route path="/course/:CourseCode" element={<CourseDetail />} />
-                <Route path="/program/:ProgramCode" element={<ProgramDetail />} />
-                <Route path="/cases" element={<SecondaryApp />} />  {/* Add this line */}
-              </Routes>
-            </main>
-          </div>
-        
+        <div className="app">
+          {!isLoginRoute && <Sidebar />}  {/* Render Sidebar only if it's not the login route */}
+          <main className="content">
+            {!isLoginRoute && <Topbar />} {/* Render Topbar only if it's not the login route */}
+            <Routes>
+              <Route path="/" element={<SecondaryApp />} />
+              <Route path="/Faculty" element={<Faculty />} />
+              <Route path="/Program" element={<Program />} />
+              <Route path="/Student" element={<Student />} />
+              <Route path="/students/:id" element={<StudentDetails />} />
+              <Route path="/Student/:id" element={<Studentpage />} />
+              <Route path="/Course" element={<Course />} />
+              <Route path="/course/:CourseCode" element={<CourseDetail />} />
+              <Route path="/program/:ProgramCode" element={<ProgramDetail />} />
+              <Route path="/cases" element={<SecondaryApp />} />  
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
