d1fe01d62ec97742ebe60e77ff0ef3ff61f97d82
rebeccaoddy <101923436+rebeccaoddy@users.noreply.github.com>
2024-09-16 01:14:01 +0200

    Update windows-start-app.bat

--- windows-start-app.bat
+++ windows-start-app.bat
@@ -1,11 +1,11 @@
 @echo off
-:: Navigate to the origin directory and install dependencies
-cd origin
-echo Installing dependencies for origin...
+:: Navigate to the caseManagementSystem directory and install dependencies
+cd caseManagementSystem
+echo Installing dependencies for caseManagementSystem...
 start cmd /k "npm install"
 
-:: Start the origin application
-echo Starting origin application...
+:: Start the caseManagementSystem application
+echo Starting caseManagementSystem application...
 start cmd /k "npm start"
 
 :: Navigate to the HonsProj-master directory and install dependencies
